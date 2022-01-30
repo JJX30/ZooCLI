4a3dc99c491e96d62d0dc821aaab828930117069
Mauricio Del Castillo <mauriciodelcastillo@Mauricios-MacBook-Pro.local>
2022-01-30 18:04:43 -0500

    ant animation, title screen

--- index.js
+++ index.js
@@ -5,22 +5,53 @@ import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import gradient from "gradient-string";
 import { nPlayer } from "@miqilin21/node-player";
-import animation from "./animation.js";
+import introFrames from "./introFrames.js";
+import titleFrames from "./titleFrames.js";
 
-const sleep = (ms = 500) => new Promise((r) => setTimeout(r, ms));
+const clearLastLines = (count) => {
+  process.stdout.moveCursor(0, -count);
+  process.stdout.clearScreenDown();
+};
 
-figlet(msg, (err, data) => {
-  const welcomeMsg = data;
-});
+const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+const fps = (ms = 300) => new Promise((r) => setTimeout(r, ms));
+const title = gradient("yellow", "orange")
+  .multiline(`d8888888P                       a88888b. dP        dP
+     .d8'                      d8'   \`88 88        88
+   .d8'   .d8888b. .d8888b.    88        88        88
+ .d8'     88'  \`88 88'  \`88    88        88        88
+d8'       88.  .88 88.  .88    Y8.   .88 88        88
+Y8888888P \`88888P' \`88888P'     Y88888P' 88888888P dP`);
 
+const jungleGradient = gradient(["#60DB0E", "#16A100"]);
+const antGradient = gradient([
+  { color: "#FF0000", pos: 0.5 },
+  { color: "#C80014", pos: 1 },
+]);
 async function welcome() {
-  console.clear();
-  console.log(animation.length);
-
-  //   intro.forEach(async (data) => {
-  //     console.log(data);
-  //     await sleep();
-  //   });
+  for (let i = 0; i < introFrames.length; i++) {
+    console.clear();
+    console.log(jungleGradient.multiline(introFrames[i]));
+    await fps();
+  }
+  await sleep();
+  for (let i = 0; i < titleFrames.length; i++) {
+    console.log(antGradient.multiline(titleFrames[i]));
+    console.log(
+      jungleGradient.multiline(
+        "oooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+      )
+    );
+    await fps();
+    clearLastLines(7);
+  }
+  console.log(chalk.yellow(title));
+  console.log(
+    jungleGradient.multiline(
+      "oooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+    )
+  );
+  await sleep();
 }
 
 await welcome();
